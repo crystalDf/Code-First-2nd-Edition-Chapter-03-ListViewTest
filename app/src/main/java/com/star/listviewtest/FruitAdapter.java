f6acde2bb908d0fe28d01d7265a2b3e54fe0bd5e
crystalDf <chendong333@gmail.com>
2017-04-16 23:12:58 +0800

    Chapter 03 ListViewTest

---
+++ app/src/main/java/com/star/listviewtest/FruitAdapter.java
@@ -0,0 +1,57 @@
+package com.star.listviewtest;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.List;
+
+
+public class FruitAdapter extends ArrayAdapter<Fruit> {
+
+    private int mResourceId;
+
+    public FruitAdapter(Context context, int resource, List<Fruit> objects) {
+        super(context, resource, objects);
+
+        mResourceId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+
+        Fruit fruit = getItem(position);
+
+        View view;
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            view = LayoutInflater.from(getContext()).inflate(mResourceId, parent, false);
+
+            viewHolder = new ViewHolder();
+            viewHolder.mFruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            viewHolder.mFruitName = (TextView) view.findViewById(R.id.fruit_name);
+
+            view.setTag(viewHolder);
+        } else {
+            view = convertView;
+            viewHolder = (ViewHolder) view.getTag();
+        }
+
+        viewHolder.mFruitImage.setImageResource(fruit.getImageId());
+        viewHolder.mFruitName.setText(fruit.getName());
+
+        return view;
+    }
+
+    private class ViewHolder {
+        private ImageView mFruitImage;
+        private TextView mFruitName;
+    }
+}
---
+++ app/src/main/java/com/star/listviewtest/MainActivity.java
@@ -0,0 +1,76 @@
+package com.star.listviewtest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ListView mListView;
+
+    private List<Fruit> mFruitList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        initFruitList();
+
+        FruitAdapter fruitAdapter = new FruitAdapter(this, R.layout.fruit_item, mFruitList);
+
+        mListView = (ListView) findViewById(R.id.list_view);
+        mListView.setAdapter(fruitAdapter);
+
+        mListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Fruit fruit = mFruitList.get(position);
+                Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void initFruitList() {
+
+        mFruitList = new ArrayList<>();
+
+        for (int i = 0; i < 3; i++) {
+            Fruit apple = new Fruit("apple", R.drawable.apple_pic);
+            mFruitList.add(apple);
+
+            Fruit banana = new Fruit("banana", R.drawable.banana_pic);
+            mFruitList.add(banana);
+
+            Fruit orange = new Fruit("orange", R.drawable.orange_pic);
+            mFruitList.add(orange);
+
+            Fruit watermelon = new Fruit("watermelon", R.drawable.watermelon_pic);
+            mFruitList.add(watermelon);
+
+            Fruit pear = new Fruit("pear", R.drawable.pear_pic);
+            mFruitList.add(pear);
+
+            Fruit grape = new Fruit("grape", R.drawable.grape_pic);
+            mFruitList.add(grape);
+
+            Fruit pineapple = new Fruit("pineapple", R.drawable.pineapple_pic);
+            mFruitList.add(pineapple);
+
+            Fruit strawberry = new Fruit("strawberry", R.drawable.strawberry_pic);
+            mFruitList.add(strawberry);
+
+            Fruit cherry = new Fruit("cherry", R.drawable.cherry_pic);
+            mFruitList.add(cherry);
+
+            Fruit mango = new Fruit("mango", R.drawable.mango_pic);
+            mFruitList.add(mango);
+        }
+    }
+}
